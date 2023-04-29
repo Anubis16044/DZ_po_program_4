ac3fb47a80ea24192a4a4f9b8e5aa245365c1add
Anubis16044 <123484313+Anubis16044@users.noreply.github.com>
2023-04-29 14:32:29 +0300

    Add files via upload

---
+++ testDB/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace testDB;
+
+internal class Program
+{
+    static void Main(string[] args) 
+    {
+        TestContext context = new TestContext();
+        User data = new User(null, "Вася", "vasia@gmail.com");
+
+        context.Users.Add(data);
+        context.SaveChanges();
+        Print(context);
+
+        data.Name = "Test";
+        context.Users.Update(data);
+        context.SaveChanges();
+        Print(context);
+
+        context.Users.Remove(data);
+        context.SaveChanges();
+        Print(context);
+        
+
+
+
+
+    }
+    static void Print(TestContext context)
+    {
+        Console.WriteLine(); 
+        foreach (var user in context.Users)
+        {
+            Console.WriteLine(user);
+        }
+    }
+}
---
+++ testDB/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testDB;
+
+public partial class User 
+{
+    public int? Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Mail { get; set; } = null!;
+    public User(int? id, string name, string mail)
+    {
+        Id = id;
+        Name = name;
+        Mail = mail;
+    }
+    public override string ToString()
+    {
+        return $"{Id}, {Name}, {Mail}";
+    }
+}
+
